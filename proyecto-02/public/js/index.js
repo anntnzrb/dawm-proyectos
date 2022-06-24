d2dd1d5b71a6027bec0d94713170651e707daa54
anntnzrb <anntnzrb@protonmail.com>
2022-06-23 22:42:19 -0500

    p02: ++avatars

--- proyecto-02/public/js/index.js
+++ proyecto-02/public/js/index.js
@@ -18,16 +18,16 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const gh_users = yield getGHUsers();
         gh_users.forEach(e => {
-            const userHTML = `
+            const user_row = `
 <tr>
   <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">${e.id}</td>
   <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">${e.login}</td>
-  <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">${e.html_url}</td>
-  <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">${e.avatar_url}</td>
+  <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400"><a href="${e.html_url}">${e.html_url}</a></td>
+  <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400"><img src="${e.avatar_url}" alt="-" width="80" height="80" /></td>
 </tr>
 `;
             const table = document.querySelector('#table-id');
-            table && (table.innerHTML += userHTML);
+            table && (table.innerHTML += user_row);
         });
     }
     catch (err) {
