8b7bc2f0a08623f60894a70e5d636290077dfda1
anntnzrb <anntnzrb@protonmail.com>
2022-07-11 17:46:42 -0500

    p02: avance

--- proyecto-02/src/js/main.js
+++ proyecto-02/src/js/main.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log(1);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const BASE_API = "https://api.github.com";
+const fetchAPI = (method, url) => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield fetch(url);
+    if (method === 'json') {
+        return yield res.json();
+    }
+    else if (method === 'xml') {
+        const data = yield res.text();
+        return new DOMParser().parseFromString(data, "application/xhtml+xml");
+    }
+});
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const iss_prs = yield fetchAPI('json', `${BASE_API}/search/issues?sort=updated&q=author:anntnzrb`);
+        const num = iss_prs.total_count;
+        console.log(iss_prs);
+        console.log(num);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}))();
--- proyecto-02/src/ts/main.ts
+++ proyecto-02/src/ts/main.ts
@@ -1,1 +1,23 @@
-console.log(1);
+const BASE_API: string = "https://api.github.com"
+
+const fetchAPI = async (method: string, url: string) => {
+    const res = await fetch(url);
+
+    if (method === 'json') {
+        return await res.json();
+    } else if (method === 'xml') {
+        const data = await res.text();
+        return new DOMParser().parseFromString(data, "application/xhtml+xml");
+    }
+}
+
+(async () => {
+    try {
+        const iss_prs = await fetchAPI('json', `${BASE_API}/search/issues?sort=updated&q=author:anntnzrb`);
+        const num = iss_prs.total_count;
+        console.log(iss_prs);
+        console.log(num);
+    } catch (err) {
+        console.error(err);
+    }
+})();
